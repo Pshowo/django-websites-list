7194a6f7632cf2dc0868325d6c382acce415eb1e
Pawel <pjuszcze@gmail.com>
2021-08-03 19:03:42 +0200

    Added: website and webpage view

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    path("webstites/", views.index, name='index' )
+    path("", views.MainPageView.as_view(), name='home'),
+    path("webstites/", views.Websites.as_view(), name="list"),
+    path("webstites/details/", views.WebPage.as_view(), name='details'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,9 +7,19 @@ from django.http.response import HttpResponse
 class MainPageView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'app/index.html')
+        return render(request, 'app/home.html', {"title":"Main"})
 
 def index(request):
     return HttpResponse("""
     <h1>Websites list</h1>
     """)
+
+class Websites(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/websites.html', {"title":"Websites"})
+
+class WebPage(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/web_detail.html', {"title":"Website detail"})        
